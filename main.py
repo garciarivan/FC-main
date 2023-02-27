c215d91479ad4c7206eb9813ed09f321bae2c4d8
garciarivan <antonio.g.rivera25@gmail.com>
2023-02-27 18:07:16 +0100

    Casi acabado

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
-
+from openpyxl.utils.dataframe import dataframe_to_rows
+from openpyxl.utils.cell import column_index_from_string
+import pandas as pd
 from PyQt5.QtWidgets import QTableWidgetItem
 from openpyxl import load_workbook
 import lib_gui
 from lib_gui import pop_up
+from docx import Document
+from docx.shared import Pt
+from docx.shared import Inches
+from docx.shared import RGBColor
+from docx.shared import Cm
+import os
+import datetime
+from mailmerge import MailMerge
+import shutil
 
+template = r'C:\Users\medico.RSD\Documents\FC-main\playground\template.docx'
 filename = 'assets\listado_pacientes.xlsx'
+ruta_carmen_ball = r'C:\Users\medico.RSD\Documents\FC-main\playground\CARMEN BALLESTEROS'
+ruta_cris_plas = r'C:\Users\medico.RSD\Documents\FC-main\playground\CRISTINA PLASENCIA'
+ruta_fab_rosby = r'C:\Users\medico.RSD\Documents\FC-main\playground\FABIOLA ROSBY'
+ruta_jose_a_fuentes = r'C:\Users\medico.RSD\Documents\FC-main\playground\JA FUENTES'
+ruta_peromingo = r'C:\Users\medico.RSD\Documents\FC-main\playground\JA PEROMINGO'
+ruta_leoncio = r'C:\Users\medico.RSD\Documents\FC-main\playground\LEONCIO BERNAL'
+ruta_angeles_perez = r'C:\Users\medico.RSD\Documents\FC-main\playground\M ANGELES PEREZ'
+ruta_remesal = r'C:\Users\medico.RSD\Documents\FC-main\playground\MARIA REMESAL'
+ruta_natalia = r'C:\Users\medico.RSD\Documents\FC-main\playground\NATALIA CLEMENTE'
+ruta_rosario = r'C:\Users\medico.RSD\Documents\FC-main\playground\ROSARIO GONZALEZ'
+ruta_verdion = r'C:\Users\medico.RSD\Documents\FC-main\playground\SUSANA VERDION'
+rutas = [ruta_natalia, ruta_leoncio, ruta_remesal, ruta_cris_plas, ruta_jose_a_fuentes, ruta_verdion, ruta_angeles_perez, ruta_carmen_ball, ruta_peromingo, ruta_rosario, ruta_fab_rosby]
+MESES = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
 
-lista_map = ['Natalia', 'Leoncio Bernal', 'Maria Remesal', 'Cristina Plasencia', 'JA Fuentes', 'Susana Verdiu',
+lista_map = ['Natalia Clemente', 'Leoncio Bernal', 'Maria Remesal', 'Cristina Plasencia', 'JA Fuentes', 'Susana Verdion',
              'M Angeles Perez', \
-             'Carmen Ballesteros', 'JA Peromingo', 'Rosario Gonzalez', 'Melisa Suelen']
+             'Carmen Ballesteros', 'JA Peromingo', 'Rosario Gonzalez', 'Fabiola Rosby']
 
 
 class Ui_MainWindow(object):
@@ -33,6 +58,7 @@ class Ui_MainWindow(object):
         self.listView.setHorizontalHeaderItem(2, item)
 
         self.load_residents()
+        
 
         self.label_name = QtWidgets.QLabel(self.centralwidget)
         self.label_name.setGeometry(QtCore.QRect(441, 286, 201, 16))
@@ -102,29 +128,37 @@ class Ui_MainWindow(object):
         self.label_2.setFont(font)
         self.label_2.setObjectName("label_2")
 
+        # boton añadir
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setGeometry(QtCore.QRect(30, 500, 121, 41))
         self.pushButton.setObjectName("pushButton")
         self.pushButton.clicked.connect(self.add_resident)
-
+        
+        # boton confirmar
         self.pushButton_add = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_add.setGeometry(QtCore.QRect(441, 500, 121, 41))
         self.pushButton_add.setObjectName("pushButton_add")
         self.pushButton_add.setVisible(False)
         self.pushButton_add.clicked.connect(self.confirm)
-
+        
+        # boton eliminar
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_2.setGeometry(QtCore.QRect(160, 500, 121, 41))
         self.pushButton_2.setObjectName("pushButton_2")
-
+        self.pushButton_2.clicked.connect(self.remove_resident)
+        
+        # boton solicitar
         self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_3.setGeometry(QtCore.QRect(632, 200, 121, 41))
         self.pushButton_3.setObjectName("pushButton_3")
         self.pushButton_3.clicked.connect(self.request)
-
+        
+        # boton resetear semana
         self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_4.setGeometry(QtCore.QRect(441, 200, 121, 41))
         self.pushButton_4.setObjectName("pushButton_4")
+        self.pushButton_4.clicked.connect(self.reset_week)
+        
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
@@ -138,6 +172,7 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+        
     def extract_cells(self):
         self.spreadsheet = load_workbook(filename)
         self.sheet = self.spreadsheet.active
@@ -145,19 +180,26 @@ class Ui_MainWindow(object):
         self.spreadsheet.save(filename)
         return rows
 
+    
     def load_residents(self):
         self.spreadsheet = load_workbook(filename)
         self.sheet = self.spreadsheet.active
-        b = 2
-        for number in range(self.sheet.max_row - 1):            
-            if str(self.sheet['A' + str(b)].value) == 'None':
-                pass
-            else:
-                self.listView.setItem(number, 0, QtWidgets.QTableWidgetItem(str(self.sheet['A' + str(b)].value)))
-                self.listView.setItem(number, 1, QtWidgets.QTableWidgetItem(
-                    self.format_date_of_birth(str(self.sheet['B' + str(b)].value))))
-                self.listView.setItem(number, 2, QtWidgets.QTableWidgetItem(str(self.sheet['C' + str(b)].value)))
-                b += 1
+        for row in self.sheet:
+            if all(cell.value is None for cell in row):
+                self.sheet.delete_rows(row[0].row, 1)
+        self.sheet.calculate_dimension()
+        self.spreadsheet.save(filename)
+        rows = self.sheet.max_row - 1
+        self.listView.setRowCount(rows)
+        self.listView.clearContents()
+        for row_num, row in enumerate(self.sheet.iter_rows(min_row=2, max_col=3)):
+            if str(row[0].value) == 'None':
+                continue
+            self.listView.setItem(row_num, 0, QTableWidgetItem(str(row[0].value)))
+            self.listView.setItem(row_num, 1, QTableWidgetItem(self.format_date_of_birth(str(row[1].value))))
+            self.listView.setItem(row_num, 2, QTableWidgetItem(str(row[2].value)))
+
+
         
         
 
@@ -169,18 +211,20 @@ class Ui_MainWindow(object):
         self.label_map.setVisible(True)
         self.combobox_map.setVisible(True)
         self.pushButton_add.setVisible(True)
-
-        # todo: escribir la logica para añadir residente con libreria openpyxl
+     
 
     def confirm(self):
+        
         self.sheet['A' + str(self.sheet.max_row + 1)] = self.textEdit_name.toPlainText()
         self.sheet['B' + str(self.sheet.max_row)] = self.textEdit_fn.toPlainText()
         self.sheet['C' + str(self.sheet.max_row)] = self.combobox_map.currentText()
         self.textEdit_name.setText('')
         self.textEdit_fn.setText('') 
+        # Ordenar las filas por orden alfabético en la primera columna
+        sort_worksheet(self.sheet, 1)
         self.spreadsheet.save(filename)
         pop_up.information(self, 'BIEN!!', 'El residente de ha añadido con éxito')
-        #self.load_residents()
+        self.load_residents()
 
     def format_date_of_birth(self, date):
         self.format1 = date.split(' ')[0]
@@ -188,15 +232,83 @@ class Ui_MainWindow(object):
         return '/'.join(self.format2[::-1])
 
     def remove_resident(self):
-        pass
-        # todo: escribir la logica para eliminar residente con libreria openpyxl, mover celdas de abajo hacia arriba
+        selected_items = self.listView.selectedItems()
+        if not selected_items:
+            pop_up.error(self, 'ATENCIÓN', 'No se ha seleccionado ningún residente.')
+            return
+        # Obtener el índice de la fila seleccionada en la tabla
+        row_index_view = self.listView.currentRow()
+        # Ajustar el índice de la fila para obtener el número de fila correspondiente en la hoja de cálculo
+        row_index_sheet = row_index_view + 2
+        # Eliminar la fila correspondiente en la hoja de Excel
+        self.spreadsheet = load_workbook(filename)
+        self.sheet = self.spreadsheet.active
+        self.sheet.delete_rows(row_index_sheet, 1)
+        self.spreadsheet.save(filename)
+        # Actualizar la tabla
+        self.load_residents()
+        pop_up.information(self, 'BIEN!!', 'El residente de ha eliminado con éxito')
+
+
 
     def request(self):
-        print(self.listView.clicked())
+        selected_items = self.listView.selectedItems()
+        if not selected_items:
+            pop_up.error(self, 'ATENCIÓN', 'No se ha seleccionado ningún residente.')
+            return
+        selected_item = self.listView.item(self.listView.currentRow(), 0).text()
+        new_row = None
+        # leer archivo Word acorde con MAP correspondiente
+        names_to_paths = {
+            'Natalia Clemente': ruta_natalia,
+            'Leoncio Bernal': ruta_leoncio,
+            'Maria Remesal': ruta_remesal,
+            'Cristina Plasencia': ruta_cris_plas,
+            'JA Fuentes': ruta_jose_a_fuentes,
+            'Susana Verdion': ruta_verdion,
+            'M Angeles Perez': ruta_angeles_perez,
+            'Carmen Ballesteros': ruta_carmen_ball,
+            'JA Peromingo': ruta_peromingo,
+            'Rosario Gonzalez': ruta_rosario,
+            'Fabiola Rosby': ruta_fab_rosby,
+        }
+
+        name = self.listView.item(self.listView.currentRow(), 2).text()
+        if name in names_to_paths:
+            ruta = os.path.join(names_to_paths[name], name.upper() + ' ' + fechado_archivo(datetime.datetime.now()) + '.docx')
+            document = Document(ruta)
+        else:
+            pop_up.error(self, 'ATENCION', 'Debe verificar ortografia de MAP')
+
+        # agregar una nueva fila a la tabla del documento
+        table = document.tables[0]
+        if table.table.cell(-1, 0).text.strip():
+            new_row = table.add_row()
+        else:
+            new_row = table.rows[-1]
+        new_row.cells[0].text = str(selected_item + '\n' + self.listView.item(self.listView.currentRow(), 1).text()).upper()
+        new_row.cells[1].text = self.textEdit_2.toPlainText().upper()
+        new_row.cells[2].text = 'DEMANDA'
+        pop_up.information(self, 'Aviso', 'Solicitud guardada correctamente.')
+        # guardar el archivo Word
+        document.save(ruta)
+
+        
 
     def reset_week(self):
-        pass
-        # todo: escribir logica para cambiar nombre y ruta de archivos con libreria sys y os
+        documento = MailMerge(template)
+        fecha_actual = datetime.datetime.now()  
+        dias_a_lunes = fecha_actual.weekday()
+        dias_a_domingo = 6 - dias_a_lunes  
+        fecha_lunes = fecha_actual - datetime.timedelta(days=dias_a_lunes)
+        fecha_domingo = fecha_actual + datetime.timedelta(days=dias_a_domingo)
+        fecha_lunes_anterior = fecha_lunes - datetime.timedelta(days=7)
+        fecha_domingo_anterior = fecha_domingo - datetime.timedelta(days=7)
+        for x, y in zip(lista_map, rutas):
+            documento.merge(map=x.upper(), lunes=str(fecha_lunes.day), domingo=str(fecha_domingo.day), mes=MESES[fecha_domingo.month -1].upper(), year=str(fecha_domingo.year))
+            documento.write(y + '\\' + x.upper() + ' ' + fechado_archivo(datetime.datetime.now()) + '.docx')
+            shutil.move(y + '\\' + x.upper() + ' ' + fechado_archivo(datetime.date(fecha_lunes_anterior.year, fecha_lunes_anterior.month, fecha_lunes_anterior.day)) + '.docx', y + '\\' + 'LISTADOS ANTIGUOS' + '\\' + x.upper() + ' ' + fechado_archivo(datetime.date(fecha_lunes_anterior.year, fecha_lunes_anterior.month, fecha_lunes_anterior.day)) + '.docx')
+        pop_up.information(self, 'HECHO!', 'Semana reseteada')
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -229,5 +341,30 @@ def main():
     sys.exit(app.exec_())
 
 
+
+def sort_worksheet(worksheet, col):
+    data = worksheet.values
+    cols = next(data)[0:]
+    df = pd.DataFrame(data, columns=cols)
+    df = df.sort_values(cols[int(col)-1])
+    for row in worksheet.iter_rows():
+        for cell in row:
+            cell.value = None
+    rows = dataframe_to_rows(df, index=False, header=True)
+    for r_idx, row in enumerate(rows, 1):
+        for c_idx, value in enumerate(row, 1):
+            worksheet.cell(row=r_idx, column=c_idx, value=value)
+
+
+def fechado_archivo(fecha):
+      
+    dias_a_lunes = fecha.weekday()
+    dias_a_domingo = 6 - dias_a_lunes  
+    fecha_lunes = fecha - datetime.timedelta(days=dias_a_lunes)
+    fecha_domingo = fecha + datetime.timedelta(days=dias_a_domingo)
+    return f'LISTADO RECETAS SEMANAL DEL {fecha_lunes.day} AL {fecha_domingo.day} DE {MESES[fecha_domingo.month -1].upper()} {fecha.year}'
+
+
 if __name__ == "__main__":
     main()
+    
--- prueba.py
+++ prueba.py
@@ -21,13 +21,79 @@ cell2.text = "Celda 2"
 # Guardar el documento
 document.save("table.docx")
 '''
-from openpyxl import load_workbook, worksheet
-filename = 'assets\listado_pacientes2.xlsx'
-spreadsheet = load_workbook(filename)
-sheet = spreadsheet.active
+import datetime
 
-a = int(input('escribe numero de columna a mover: '))
-worksheet.worksheet.Worksheet.move_range(cell_range="A1:C1", rows=0, cols=a)
+'''def fechado_archivo():
+    #fecha_actual = datetime.datetime.now()    
+    fecha_actual = datetime.date(2023, 2, 9)
+    MESES = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+    if fecha_actual.weekday() == 0:
+        duracion = datetime.timedelta(days=6)
+        fecha_futura = fecha_actual + duracion
+        return 'LISTADO RECETAS SEMANAL DEL {} AL {} DE {} {}'.format(fecha_actual.day, fecha_futura.day, MESES[fecha_futura.month - 1].upper(), fecha_futura.year)
+    elif fecha_actual.weekday() == 1:
+        duracion = datetime.timedelta(days=5)
+        fecha_futura = fecha_actual + duracion
+        return 'LISTADO RECETAS SEMANAL DEL {} AL {} DE {} {}'.format(fecha_actual.day - 1, fecha_futura.day, MESES[fecha_futura.month - 1].upper(), fecha_futura.year)
+    elif fecha_actual.weekday() == 2:
+        duracion = datetime.timedelta(days=4)
+        fecha_futura = fecha_actual + duracion
+        return 'LISTADO RECETAS SEMANAL DEL {} AL {} DE {} {}'.format(fecha_actual.day - 2, fecha_futura.day, MESES[fecha_futura.month - 1].upper(), fecha_futura.year)
+    elif fecha_actual.weekday() == 3:
+        duracion = datetime.timedelta(days=3)
+        fecha_futura = fecha_actual + duracion
+        return 'LISTADO RECETAS SEMANAL DEL {} AL {} DE {} {}'.format(fecha_actual.day - 3, fecha_futura.day, MESES[fecha_futura.month - 1].upper(), fecha_futura.year)
+    elif fecha_actual.weekday() == 4:
+        duracion = datetime.timedelta(days=2)
+        fecha_futura = fecha_actual + duracion
+        return 'LISTADO RECETAS SEMANAL DEL {} AL {} DE {} {}'.format(fecha_actual.day - 4, fecha_futura.day, MESES[fecha_futura.month - 1].upper(), fecha_futura.year)
+    elif fecha_actual.weekday() == 5:
+        duracion = datetime.timedelta(days=1)
+        fecha_futura = fecha_actual + duracion
+        return 'LISTADO RECETAS SEMANAL DEL {} AL {} DE {} {}'.format(fecha_actual.day - 5, fecha_futura.day, MESES[fecha_futura.month - 1].upper(), fecha_futura.year)
+    elif fecha_actual.weekday() == 6:
+        duracion = datetime.timedelta(days=0)
+        fecha_futura = fecha_actual + duracion
+        return 'LISTADO RECETAS SEMANAL DEL {} AL {} DE {} {}'.format(fecha_actual.day - 6, fecha_futura.day, MESES[fecha_futura.month - 1].upper(), fecha_futura.year)
 
+print(fechado_archivo())'''
+'''def fechado_archivo():
+    fecha_actual = datetime.datetime.now()
+    MESES = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+    dias = {0: 6, 1: 5, 2: 4, 3: 3, 4: 2, 5: 1, 6: 0}
+    duracion = datetime.timedelta(days=dias[fecha_actual.weekday()])
+    fecha_futura = fecha_actual + duracion
+    return 'LISTADO RECETAS SEMANAL DEL {} AL {} DE {} {}'.format(fecha_actual.day - dias[fecha_actual.weekday()], fecha_futura.day, MESES[fecha_futura.month - 1].upper(), fecha_futura.year)
+print(fechado_archivo())'''
+'''from dateutil.relativedelta import relativedelta
+import calendar
 
-print('Hecho')
+def fechado_archivo():
+    #fecha_actual = datetime.datetime.now()
+    fecha_actual = datetime.date(2023, 2, 9) 
+    MESES = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+    dias_mes = calendar.monthrange(fecha_actual.year, fecha_actual.month)[1]
+    fecha_futura = fecha_actual + datetime.timedelta(days=6)
+    if fecha_futura.day > dias_mes:
+        fecha_futura = fecha_futura.replace(month=fecha_futura.month+1, day=1)
+    return 'LISTADO RECETAS SEMANAL DEL {} AL {} DE {} {}'.format(fecha_actual.day, fecha_futura.day, MESES[fecha_futura.month - 1].upper(), fecha_futura.year)
+print(fechado_archivo())'''
+
+'''#import calendar
+MESES = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+def fechado_archivo():
+    fecha_actual = datetime.datetime.now()  
+    dias_a_lunes = fecha_actual.weekday()
+    dias_a_domingo = 6 - dias_a_lunes  
+    fecha_lunes = fecha_actual - datetime.timedelta(days=dias_a_lunes)
+    fecha_domingo = fecha_actual + datetime.timedelta(days=dias_a_domingo)
+    return f'LISTADO RECETAS SEMANAL DEL {fecha_lunes.day} AL {fecha_domingo.day} DE {MESES[fecha_domingo.month -1].upper()} {fecha_actual.year}'
+print(fechado_archivo())'''
+
+from mailmerge import MailMerge
+template = r'C:\Users\medico.RSD\Documents\FC-main\playground\template.docx'
+document = MailMerge(template)
+
+document.merge(map='Carmen Ballesteros'.upper(), lunes=str(27), domingo=str(5), mes='Marzo'.upper(), year=str(2023))
+document.write(r'C:\Users\medico.RSD\Documents\FC-main\playground\prueba.docx')
+print('HECHO!')
